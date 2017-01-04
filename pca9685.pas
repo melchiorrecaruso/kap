efcfd67e7b0dbd619ea86c5ab3a4178116d1b998
Melchiorre Caruso <melchiorrecaruso@gmail.com>
2017-01-04 14:11:05 +0100

    PCA9685
    First release PCA9685 unit

--- pca9685.pas
+++ pca9685.pas
@@ -9,17 +9,59 @@ uses
   wiringPi;
 
 const
+
+  // PCA9685 Registers
+
+  PCA9685_MODE1         = $00;
+  PCA9685_MODE2         = $01;
+  PCA9685_SUBADR1       = $02;
+  PCA9685_SUBADR2       = $03;
+  PCA9685_SUBADR3       = $04;
+  PCA9685_ALLCALLADR    = $05;
+  PCA9685_LED0_ON_L     = $06;
+  PCA9685_LED0_ON_H     = $07;
+  PCA9685_LED0_OFF_L    = $08;
+  PCA9685_LED0_OFF_H    = $09;
+
+  // LED1 .. LED15 (0x0A .. 0x45)
+
+  PCA9685_ALL_LED_ON_L  = $FA;
+  PCA9685_ALL_LED_ON_H  = $FB;
+  PCA9685_ALL_LED_OFF_L = $FC;
+  PCA9685_ALL_LED_OFF_H = $FD;
+  PCA9685_PRE_SCALE     = $FE;
+  PCA9685_TESTMODE      = $FF;
+
+  // Bits in the MODE1 register
+
+  PCA9685_MODE1_RESTART = $80;
+  PCA9685_MODE1_EXTCLK  = $40;
+  PCA9685_MODE1_AI      = $20;
+  PCA9685_MODE1_SLEEP   = $10;
+  PCA9685_MODE1_SUB1    = $08;
+  PCA9685_MODE1_SUB2    = $04;
+  PCA9685_MODE1_SUB3    = $02;
+  PCA9685_MODE1_ALLCALL = $01;
+
+  // Bits in the MODE2 register
+
+  PCA9685_MODE2_INVRT   = $10;
+  PCA9685_MODE2_OCH     = $08;
+  PCA9685_MODE2_OUTDRV  = $04;
+  PCA9685_MODE2_OUTNE_1 = $02;
+  PCA9685_MODE2_OUTNE_0 = $01;
+
+  PCA9685_OSC_CLOCK     = 25000000.0;
+  PCA9685_PRESCALE_DIV  = 4096.0;
+
+const
   PIN_ALL = 16;
 
-  // Setup registers
-  PCA9685_MODE1     = $0;
-  PCA9685_PRESCALE  = $FE;
-
-
+  function pca9685Setup(pinBase: longint; i2cAddress: longint; freq: longint): longint;
 
 implementation
 
-procedure myPwmWrite (node: pwiringPiNodeStruct; pin: longint; value: longint);
+procedure pca9685PwmWrite(node: pwiringPiNodeStruct; pin: longint; value: longint); cdecl;
 var
   fd: longint;
   chan: longint;
@@ -27,52 +69,68 @@ begin
   fd   := node^.fd;
   chan := 4 * (pin - node^.pinBase);
 
-  wiringPiI2CWriteReg8(fd, LED0_ON_L  + chan, $00);
-  wiringPiI2CWriteReg8(fd, LED0_ON_H  + chan, $00);
-  wiringPiI2CWriteReg8(fd, LED0_OFF_L + chan, value and $FF);
-  wiringPiI2CWriteReg8(fd, LED0_OFF_H + chan, (value shr 8) and $FF);
+  wiringPiI2CWriteReg8(fd, PCA9685_LED0_ON_L  + chan, $00);
+  wiringPiI2CWriteReg8(fd, PCA9685_LED0_ON_H  + chan, $00);
+  wiringPiI2CWriteReg8(fd, PCA9685_LED0_OFF_L + chan, (value      ) and $FF);
+  wiringPiI2CWriteReg8(fd, PCA9685_LED0_OFF_H + chan, (value shr 8) and $FF);
 end;
 
-
-function pca9685Setup(pinBase: longint; i2cAddress: longint; freq: real): longint;
-var
-  node: pwiringPiNodeStruct;
-  fd: longint;
-  settings: longint;
-  autoInc: longint;
+procedure pca9685PwmReset(fd: longint); cdecl;
 begin
-  // Create a node with 16 pins [0..15] + [16] for all
-  node := wiringPiNewNode(pinBase, PIN_ALL + 1);
+  wiringPiI2CWriteReg8(fd, PCA9685_ALL_LED_ON_L,  $00);
+  wiringPiI2CWriteReg8(fd, PCA9685_ALL_LED_ON_H,  $00);
+  wiringPiI2CWriteReg8(fd, PCA9685_ALL_LED_OFF_L, $00);
+  wiringPiI2CWriteReg8(fd, PCA9685_ALL_LED_OFF_H, $00);
+end;
 
-  // Check if pinBase is available
+procedure pca9685PWMfreq(fd: longint; freq: longint);
+var
+  prescale: longint;
+  mode1: longint;
+begin
+  // Calculate presacle
+  prescale := round(PCA9685_OSC_CLOCK / (PCA9685_PRESCALE_DIV * freq)) - 1;
+  // Read MODE1 register
+  mode1 := wiringPiI2CReadReg8(fd, PCA9685_MODE1);
+  // Write MODE1 register (discard RESTART bit and set SLEEP bit)
+  wiringPiI2CWriteReg8(fd, PCA9685_MODE1, (mode1 and $7F) or PCA9685_MODE1_SLEEP);
+  // Write PRE_SCALE register
+  wiringPiI2CWriteReg8(fd, PCA9685_PRE_SCALE, prescale);
+  // Restore MODE1 register
+  wiringPiI2CWriteReg8(fd, PCA9685_MODE1, mode1);
+  delay(5);
+  // Write MODE1 register (set RESTART BIT)
+  wiringPiI2CWriteReg8(fd, PCA9685_MODE1, mode1 or PCA9685_MODE1_RESTART);
+end;
+
+function pca9685Setup(pinBase: longint; i2cAddress: longint; freq: longint): longint;
+var
+  fd: longint;
+  node: pwiringPiNodeStruct;
+begin
   result := -1;
-  if node <> nil then
+  fd := wiringPiI2CSetup(i2cAddress);
+  if fd <> -1 then
   begin
-    // Check i2c address
-    fd := wiringPiI2CSetup(i2cAddress);
-    if (fd < 0) then exit;
+    wiringPiI2CWriteReg8(fd, PCA9685_MODE1, $00);
+    wiringPiI2CWriteReg8(fd, PCA9685_MODE2, $04);
 
-    // Setup the chip. Enable auto-increment of registers.
-    settings := wiringPiI2CReadReg8(fd, PCA9685_MODE1) and $7F;
-    autoInc  := settings or $20;
+    pca9685PWMreset(fd);
+    pca9685PWMfreq (fd, freq);
 
-    wiringPiI2CWriteReg8(fd, PCA9685_MODE1, autoInc);
-
-    // Set frequency of PWM signals. Also ends sleep mode and starts PWM output.
-    if freq > 0 then
-      pca9685PWMFreq(fd, freq);
+    node := wiringPiNewNode(pinBase, 16);
 
     node^.fd	       := fd;
-    node->pwmWrite     := @myPwmWrite;
-    node->digitalWrite := @myOnOffWrite;
-    node->digitalRead  := @myOffRead;
-    node->analogRead   := @myOnRead;
+    node^.pwmWrite     := @pca9685PwmWrite;
+  //node^.digitalWrite := @myOnOffWrite;
+  //node^.digitalRead  := @myOffRead;
+  //node^.analogRead   := @myOnRead;
 
-    result := fd;
+    result := 0;
   end;
 end;
 
-}
+
 
 
 end.
